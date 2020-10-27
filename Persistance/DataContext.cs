322790a1fa4965b03163ae3bfbdf66e0e511b579
Suyati <suy1@suyati.com>
2020-10-27 16:01:58 +0530

    MediatR Pattern Completed

--- Persistance/DataContext.cs
+++ Persistance/DataContext.cs
@@ -7,9 +7,10 @@ namespace Persistance
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions options) : base(options)
-        {
-            
+        {            
         }
+
+        public DbSet<Activity> Activities { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder){
             builder.Entity<Value>().
